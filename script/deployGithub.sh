56f02be9aac32a8a06466501c89e7624ec2176e0
nolte <nolte07@gmail.com>
2016-07-16 17:05:05 +0200

    Update deployGithub.sh

--- script/deployGithub.sh
+++ script/deployGithub.sh
@@ -40,6 +40,8 @@ git commit -m "Deploy to GitHub Pages: ${SHA}"
 echo $(pwd)
 echo ls -all
 
+cd ./settings/keys
+
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
