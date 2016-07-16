eafeb84bb55ab27878db4f06e0809ceef97dda52
nolte <nolte07@gmail.com>
2016-07-16 17:28:34 +0200

    Update deployGithub.sh

--- script/deployGithub.sh
+++ script/deployGithub.sh
@@ -9,7 +9,7 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 cd ..
-BASEDIR = $(pwd)
+BASEDIR=`echo $(pwd)`
 
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
