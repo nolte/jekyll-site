bb45df8878c543badb4280e3e1098bb075db50a7
nolte <nolte@ingo.de>
2016-07-19 13:24:30 +0200

    switch to date format

--- script/keen-io-count.sh
+++ script/keen-io-count.sh
@@ -3,6 +3,9 @@ set -e # halt script on error
 
 build_start=$4
 build_end=$5
+build_end_formatted=$(date -d @${build_end}  +"%Y-%m-%dT%H:%H:%M:%S.%3NZ")
+build_start_formatted=$(date -d @${build_start}  +"%Y-%m-%dT%H:%H:%M:%S.%3NZ")
+
 duration=$((build_end-build_start)) 
 project=$3
 cisystem=$1
@@ -13,5 +16,5 @@ test_result=$6
 curl -s -X POST\
         -H "Content-Type: application/json"\
         -H "Accept: application/json"\
-        -d '{"cisystem":"'${cisystem}'","project":"'${project}'","build_start":'${build_start}',"build_end":'${build_end}',"branche":"'${branche}'","test_result":'${test_result}',"duration":'${duration}'}'\
+        -d '{"cisystem":"'${cisystem}'","project":"'${project}'","build_start":"'${build_start_formatted}'","build_end":"'${build_end_formatted}'","branche":"'${branche}'","test_result":'${test_result}',"duration":'${duration}'}'\
         https://api.keen.io/3.0/projects/578d609a80a7bd79bfd6cb0a/events/build?api_key=${keen_api_key}
