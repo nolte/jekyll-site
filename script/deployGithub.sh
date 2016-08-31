706199106d6a2a071f01b5eeb084811bfe3255f3
nolte <nolte07@gmail.com>
2016-08-31 18:22:32 +0200

    Update deployGithub.sh
    add

--- script/deployGithub.sh
+++ script/deployGithub.sh
@@ -11,6 +11,9 @@ SHA=`git rev-parse --verify HEAD`
 cd ..
 BASEDIR=`echo $(pwd)`
 
+git config --global user.email "nolte07@googlemail.com"
+git config --global user.name "TravisCi"
+
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
 git clone $REPO out
